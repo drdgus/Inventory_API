b1ee46fa6724483b51e3ce2f25c3de95805a3b60
Даниил Гусевский <dr.dgus@yandex.ru>
2021-04-06 16:50:26 +0700

    Добавьте файлы проекта.

---
+++ Inventory_API/Controllers/InventoryController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Inventory_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class InventoryController : ControllerBase
+    {
+        private readonly ILogger<InventoryController> _logger;
+
+        public InventoryController(ILogger<InventoryController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<> Get()
+        {
+
+            return ;
+        }
+    }
+}
---
+++ Inventory_API/Models/Equip.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Inventory_API.Models
+{
+    public class Equip
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string InvNum { get; set; }
+        public int OrgId { get; set; }
+        public int RoomId { get; set; }
+        public int TypeId { get; set; }
+        public int StatusId { get; set; }
+        public int AccountabilityId { get; set; }
+        public string Note { get; set; }
+        public int Count { get; set; }
+    }
+}
